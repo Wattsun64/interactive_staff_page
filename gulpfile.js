8720eab70ed9cb486d472140a8ac6746b7b69ece
Joshua Colvin <joshua@joshuacolvin.net>
2017-01-23 18:28:34 -0500

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,28 @@
 const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
 
-gulp.default()
+gulp.task('sass', () => {
+  console.log('sass called')
+  return gulp
+    .src('styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('styles/'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    server: './'
+  })
+
+  gulp.watch('styles/*.scss', ['sass'])
+  gulp.watch('./*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['sass', 'serve'])
